f0d45d96406cc3e13ebce0705973c83e4b8de883
Gabriel Batista <svbgabriel@gmail.com>
2019-11-14 19:09:53 -0300

    Converte os componentes para funções

--- src/App.js
+++ src/App.js
@@ -1,36 +1,7 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import "./style.css";
 
 import Todo from "./components/Todo";
-
-export default class App extends Component {
-  state = {
-    todos: []
-  };
-
-  addTodo = event => {
-    if (event.key === "Enter") {
-      this.setState({
-        todos: [
-          ...this.state.todos,
-          { id: Math.random(), text: event.target.value }
-        ]
-      });
-    }
-  };
-
-  render() {
-    return (
-      <div style={styles.container}>
-        <h1>Todos</h1>
-        <input onKeyPress={this.addTodo} />
-        {this.state.todos.map(todo => (
-          <Todo key={todo.id} title={todo.text} />
-        ))}
-      </div>
-    );
-  }
-}
 
 const styles = {
   container: {
@@ -40,3 +11,23 @@ const styles = {
     alignItems: "center"
   }
 };
+
+export default function App() {
+  const [todos, setTodos] = useState([]);
+
+  function addTodo(event) {
+    if (event.key === "Enter") {
+      setTodos([...todos, { id: Math.random(), text: event.target.value }]);
+    }
+  }
+
+  return (
+    <div style={styles.container}>
+      <h1>Todos</h1>
+      <input onKeyPress={addTodo} />
+      {todos.map(todo => (
+        <Todo key={todo.id} title={todo.text} />
+      ))}
+    </div>
+  );
+}
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,11 +1,13 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const Todo = ({ title }) => (
-  <div>
-    <h1>{title}</h1>
-  </div>
-);
+function Todo({ title }) {
+  return (
+    <div>
+      <h1>{title}</h1>
+    </div>
+  );
+}
 
 Todo.defaultProps = {
   title: "Todo padrão"
