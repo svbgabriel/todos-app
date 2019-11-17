23f134bb5ebd8310f428ae74ada378c66a529344
Gabriel Batista <svbgabriel@gmail.com>
2019-11-16 22:04:23 -0300

    Estiliza a aplicação

--- src/App.js
+++ src/App.js
@@ -8,7 +8,47 @@ const styles = {
     display: "flex",
     flexDirection: "column",
     justifyContent: "center",
-    alignItems: "center"
+    alignItems: "center",
+    margin: "50px auto"
+  },
+  containerTodos: {
+    background: "#fff",
+    minWidth: "350px",
+    maxWidth: "500px",
+    padding: "10px"
+  },
+  header: {
+    width: "100%"
+  },
+  footer: {
+    display: "flex",
+    justifyContent: "space-between",
+    marginTop: "10px"
+  },
+  containerButtons: {
+    display: "flex",
+    margin: "0 15px"
+  },
+  input: {
+    border: "none",
+    height: "32px",
+    fontSize: "24px",
+    width: "100%",
+    padding: "20px",
+    boxSizing: "border-box",
+    outlineWidth: "0"
+  },
+  separator: {
+    height: "1px",
+    background: "#c1c1c1"
+  },
+  title: {
+    fontSize: "48px"
+  },
+  removeButton: {
+    border: "0",
+    outlineWidth: "0",
+    background: "none"
   }
 };
 
@@ -68,21 +108,71 @@ export default function App() {
 
   return (
     <div style={styles.container}>
-      <h1>Todos</h1>
-      <input onKeyPress={addTodo} />
-      {filteredTodos.map(todo => (
-        <Todo
-          key={todo.id}
-          todo={todo}
-          remove={() => deleteTodo(todo.id)}
-          update={() => updateTodo(todo.id)}
-        />
-      ))}
-      <p>{`Faltam ${notDoneCount} tarefas`}</p>
-      <button onClick={() => setFilter("all")}>Todos</button>
-      <button onClick={() => setFilter("notdone")}>Ativos</button>
-      <button onClick={() => setFilter("done")}>Completos</button>
-      <button onClick={() => deleteDoneTodos()}>Remover Completos</button>
+      <h1 style={styles.title}>tarefas</h1>
+      <div style={styles.containerTodos}>
+        <div style={styles.header}>
+          <input
+            style={styles.input}
+            placeholder="O que precisa ser feito?"
+            onKeyPress={addTodo}
+          />
+        </div>
+        <div style={styles.separator} />
+        {filteredTodos.map(todo => (
+          <Todo
+            key={todo.id}
+            todo={todo}
+            remove={() => deleteTodo(todo.id)}
+            update={() => updateTodo(todo.id)}
+          />
+        ))}
+        {todos.length !== 0 && (
+          <div style={styles.footer}>
+            <p>{`Faltam ${notDoneCount} tarefa(s)`}</p>
+            <div style={styles.containerButtons}>
+              <button
+                style={{
+                  outlineWidth: "0",
+                  background: "none",
+                  borderRadius: "5px",
+                  borderWidth: filter === "all" ? "2px" : "0"
+                }}
+                onClick={() => setFilter("all")}
+              >
+                Todos
+              </button>
+              <button
+                style={{
+                  outlineWidth: "0",
+                  background: "none",
+                  borderRadius: "5px",
+                  borderWidth: filter === "notdone" ? "2px" : "0"
+                }}
+                onClick={() => setFilter("notdone")}
+              >
+                Ativos
+              </button>
+              <button
+                style={{
+                  outlineWidth: "0",
+                  background: "none",
+                  borderRadius: "5px",
+                  borderWidth: filter === "done" ? "2px" : "0"
+                }}
+                onClick={() => setFilter("done")}
+              >
+                Completos
+              </button>
+            </div>
+            <button
+              style={styles.removeButton}
+              onClick={() => deleteDoneTodos()}
+            >
+              Remover completos
+            </button>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -3,17 +3,40 @@ import PropTypes from "prop-types";
 
 const styles = {
   container: {
-    display: "flex"
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center"
+  },
+  separator: {
+    height: "1px",
+    background: "#c1c1c1"
+  },
+  button: {
+    background: "none",
+    border: "0",
+    outlineWidth: "0"
   }
 };
 
 export default function Todo({ todo, remove, update }) {
   return (
-    <div style={styles.container}>
-      <button onClick={update}>U</button>
-      <p>{todo.text}</p>
-      <button onClick={remove}>X</button>
-    </div>
+    <>
+      <div style={styles.container}>
+        <input type="checkbox" defaultChecked={!todo.active} onClick={update} />
+        <p
+          style={{
+            textDecoration: todo.active ? "none" : "line-through",
+            fontSize: "24px"
+          }}
+        >
+          {todo.text}
+        </p>
+        <button style={styles.button} onClick={remove}>
+          X
+        </button>
+      </div>
+      <div style={styles.separator} />
+    </>
   );
 }
 
